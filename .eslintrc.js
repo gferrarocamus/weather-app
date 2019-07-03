483729393d060fc88185251cd8b4988fddb44172
Giuliana Ferraro <giulianaferrarocamus@gmail.com>
2019-07-03 12:42:39 +0200

    initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb-base'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/no-cycle': 'off',
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'prefer-destructuring': 'off',
+    'no-alert': 'off',
+    'no-restricted-globals': 'off',
+  }
+};
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # weather-app
+
+> This app allows you to get current weather details for a city of your interest.
+
+The data is retrieved from [OpenWeather](https://openweathermap.org/).
+
+See it live [here](https://gferrarocamus.github.io/restaurant-page/).
+
+## Contributors
+
+[@gferrarocamus](https://github.com/gferrarocamus)
+
+## License
+
+MIT © 2019 @gferrarocamus
+
+b8d08b4b5bc310505709e7342891ec46
